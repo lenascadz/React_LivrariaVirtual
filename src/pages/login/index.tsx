913193282ba7c58d112ad16dd7c8c7e0940e4a47
Helena Scandian Trinta <hscandian@id.uff.br>
2025-03-03 00:56:23 -0300

    feat: criando login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,34 @@
-import Header from "../../components/header";
+
+import styles from './styles.module.css';
+
 
 export default function Login() {
     return (
-        <>
-            <Header/>  
+        <> 
                 
             <main>
-                <h2> login page </h2>
+                <div className={styles.capa}>
+                    <div className={styles.foto}>
+                        <img src="./src/assets/capa_login.png" alt="" /> 
+                    </div> 
+                    <div className={styles.infos}>
+                        <div className={styles.logo}>
+                            <img src="./src/assets/logo.png"/>
+                        </div>
+                        <div className={styles.saudacao}> 
+                            <h2> Bem vindo (a)! </h2> 
+                            <h1> Entre na sua conta </h1>           
+                        </div>
+                        <div className={styles.form}>
+                            <h2>E-mail</h2>
+                            <textarea placeholder="Digite aqui seu e-mail:"/>
+                            <h2>Senha</h2>
+                            <textarea placeholder="Digite aqui sua senha:"/>
+                            <button> entrar </button> 
+
+                        </div>
+                    </div>
+                </div>
             </main>
         </>
     )
